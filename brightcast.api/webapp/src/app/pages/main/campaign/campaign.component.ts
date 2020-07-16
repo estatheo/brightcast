896a01ff8f6f76c6e1799feefc658810a9951e2f
topjobstar0605 <topjobstar@gmail.com>
2020-07-16 18:37:00 +1000

    campaign contactlist dropdown not updated? - fixed

--- brightcast.api/webapp/src/app/pages/main/campaign/campaign.component.ts
+++ brightcast.api/webapp/src/app/pages/main/campaign/campaign.component.ts
@@ -10,51 +10,60 @@ import { CampaignData } from '../../_models/campaignData';
 @Component({
   selector: 'ngx-campaign',
   templateUrl: './campaign.component.html',
-  styleUrls: ['./campaign.component.scss']
+  styleUrls: ['./campaign.component.scss'],
 })
 export class CampaignComponent implements OnInit {
 
-  constructor(private windowService: NbWindowService, private toastrService: NbToastrService, private campaignService: CampaignService, private campaignsService: CampaignService) { }
+  constructor(
+    private windowService: NbWindowService,
+    private toastrService: NbToastrService,
+    private campaignService: CampaignService,
+    private campaignsService: CampaignService) { }
   data: any;
   ngOnInit(): void {
+    this.campaignsService.refreshData();
     this.campaignsService.data.subscribe((data: CampaignData) => {
       this.data = data;
     });
   }
-  
+
   openModal() {
-    this.windowService.open(CampaignNewComponent, { title: 'New Campaign', context: { contactListList: this.data.contactLists} });
+    this.windowService.open(
+      CampaignNewComponent,
+      { title: 'New Campaign', context: { contactListList: this.data.contactLists} });
   }
-  
-  openModalForEdit(event) {   
+
+  openModalForEdit(event) {
     this.campaignsService.data.subscribe((data: CampaignData) => {
       this.data = data;
-      this.windowService.open(CampaignFormComponent, { title: 'Edit Campaign', context: { contactListList: this.data.contactLists, campaign: event } });
+      this.windowService.open(
+        CampaignFormComponent,
+        { title: 'Edit Campaign', context: { contactListList: this.data.contactLists, campaign: event } });
     });
-    
+
   }
 
   delete(id) {
     this.campaignsService.Delete(id).subscribe(() => {
-      this.toastrService.primary("❌ The campaign has been deleted!", "Deleted!");
+      this.toastrService.primary('❌ The campaign has been deleted!', 'Deleted!');
       this.campaignsService.refreshData();
       this.campaignsService.requestData().subscribe((data: CampaignData) => {
         this.data = data;
       });
     }, error => {
-      this.toastrService.danger("⚠ There was an error processing the request!", "Error!");
-    })
+      this.toastrService.danger('⚠ There was an error processing the request!', 'Error!');
+    });
   }
 
   send(campaign) {
     this.campaignsService.SendCampaign(campaign).subscribe(result => {
-      this.toastrService.primary("🎉 The campaign has been sent!", "Success!");
+      this.toastrService.primary('🎉 The campaign has been sent!', 'Success!');
       this.campaignsService.refreshData();
       this.campaignsService.data.subscribe((data: CampaignData) => {
         this.data = data;
       });
     }, error => {
-      this.toastrService.danger("⚠ There was an error processing the request!", "Error!");
+      this.toastrService.danger('⚠ There was an error processing the request!', 'Error!');
     });
   }
 }
