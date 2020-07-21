cd6f06b9707787835d61ddcc7c33b15232f64aa7
topjobstar0605 <topjobstar@gmail.com>
2020-07-21 22:12:56 +0900

    loading spinner on save btns in contactlist and contact page

--- brightcast.api/webapp/src/app/pages/main/contact/contact-form/contact-form.component.ts
+++ brightcast.api/webapp/src/app/pages/main/contact/contact-form/contact-form.component.ts
@@ -6,7 +6,7 @@ import { ContactService } from '../../../../@core/apis/contact.service';
 
 
 @Component({
-    template: 
+    template:
     `<form class="form" [formGroup]="form">
     <div class="form-group">
         <label for="name" class="label">Contact First Name</label>
@@ -29,7 +29,7 @@ import { ContactService } from '../../../../@core/apis/contact.service';
         <br/>
         <nb-checkbox id="subscribed" value="true" formControlName="subscribed"></nb-checkbox>
     </div>
-    <button type="submit" style="margin-top: 10px" nbButton status="primary" (click)="onSubmit()">Save</button>
+    <button [nbSpinner]="loading" nbSpinnerStatus="success" type="submit" style="margin-top: 10px" nbButton status="primary" (click)="onSubmit()">Save</button>
     </form>`,
     styleUrls: ['contact-form.component.scss'],
 })
@@ -37,35 +37,48 @@ export class ContactFormComponent {
     contactListId;
     event;
     form;
-    constructor(private router: Router, private formBuilder: FormBuilder, public windowRef: NbWindowRef, private toastrService: NbToastrService, private contactService: ContactService) {
+    loading = false;
+    constructor(
+      private router: Router,
+      private formBuilder: FormBuilder,
+      public windowRef: NbWindowRef,
+      private toastrService: NbToastrService,
+      private contactService: ContactService) {
         this.form = this.formBuilder.group({
           firstName: ['', Validators.required],
           lastName: ['', Validators.required],
           phone: ['', Validators.required],
           email: ['', Validators.required],
-          subscribed: ['', Validators.required]
+          subscribed: ['', Validators.required],
           });
     }
 
     onSubmit() {
-        this.contactService.NewContact({
-          firstName: this.form.controls.firstName.value,
-          lastName: this.form.controls.lastName.value,
-          phone: this.form.controls.phone.value,
-          email: this.form.controls.email.value,
-          subscribed: this.form.controls.subscribed.value,
-          contactListId: parseInt(this.contactListId)
-        }).subscribe(() => {
-          this.toastrService.success("🚀 The Contact has been added!", "Success!");
-          this.contactService.refreshData();
-          this.router.navigateByUrl('/',{skipLocationChange: true}).then(() => {
-            this.router.navigate([`/pages/main/customer-list/${this.contactListId}/contacts`]);
-            this.close();
-          });
-        }, error => {
-          this.toastrService.danger(error, "There was an error on our side😢");
+      this.loading = true;
+
+      this.contactService.NewContact({
+        firstName: this.form.controls.firstName.value,
+        lastName: this.form.controls.lastName.value,
+        phone: this.form.controls.phone.value,
+        email: this.form.controls.email.value,
+        subscribed: this.form.controls.subscribed.value,
+        contactListId: parseInt(this.contactListId, 10),
+      }).subscribe(() => {
+        this.toastrService.success('🚀 The Contact has been added!', 'Success!');
+        this.contactService.refreshData();
+        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+          this.router.navigate([`/pages/main/customer-list/${this.contactListId}/contacts`]);
+          this.close();
         });
-      }
+      }, error => {
+        this.loading = false;
+        this.toastrService.danger(error, 'There was an error on our side😢');
+      });
+
+      setTimeout(() => {
+        this.loading = false;
+      }, 7000);
+    }
 
     close() {
         this.windowRef.close();
