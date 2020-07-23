b3f61a914383b84c5de05ea27fe68bc7e27d5d81
topjobstar0605 <topjobstar@gmail.com>
2020-07-23 22:44:01 +0900

    confirmation email for signup redirect to localhost - fixed

--- brightcast.api/webapp/src/app/custom/auth/reset/reset.component.ts
+++ brightcast.api/webapp/src/app/custom/auth/reset/reset.component.ts
@@ -6,41 +6,33 @@ import { NbToastrService } from '@nebular/theme';
 @Component({
   selector: 'ngx-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.scss']
+  styleUrls: ['./reset.component.scss'],
 })
 export class ResetComponent implements OnInit {
 
-  authError: boolean = false;
-  alertText = '';
   loading = false;
   submitted = false;
-  
+
   constructor(private accountService: AccountService,
     private alertService: AlertService,
-    private router: Router,    
+    private router: Router,
     private route: ActivatedRoute,
     private toastrService: NbToastrService) { }
 
   ngOnInit(): void {
   }
 
-  onClose() {
-    this.authError = false;
-    this.alertText = '';
-  }
-
   reset(email) {
-    this.accountService.requestPasswordReset(email).subscribe(data => {                    
-      this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+    this.loading = true;
+
+    this.accountService.requestPasswordReset(email).subscribe(data => {
+      this.toastrService.success('We have sent you an email. Please reset password.', 'Success', {duration: 7000});
       this.router.navigate(['../../login'], { relativeTo: this.route });
-  },
-  error => {
-      this.toastrService.danger("❌ There was an error with the request!", "Error!");
-      this.alertText = error;
-        this.authError = true;
-        setTimeout(() =>  this.onClose(), 2000);
-      this.alertService.error(error);
       this.loading = false;
-  });
+    },
+    error => {
+        this.toastrService.danger('❌ ' + error, 'Error');
+        this.loading = false;
+    });
   }
 }
