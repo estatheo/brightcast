341b98f9d9be2a77722a80f399f78fcb2dd0fe4b
topjobstar0605 <topjobstar@gmail.com>
2020-07-24 16:51:49 +0900

    alert in pages - fixed

--- brightcast.api/webapp/src/app/custom/auth/login/login.component.ts
+++ brightcast.api/webapp/src/app/custom/auth/login/login.component.ts
@@ -2,60 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { NbLoginComponent, NbAuthService } from '@nebular/auth';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService, AlertService } from '../../../pages/_services';
+import { AccountService } from '../../../pages/_services';
+import { NbToastrService } from '@nebular/theme';
 import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent extends NbLoginComponent {
+export class LoginComponent extends NbLoginComponent implements OnInit {
   form: any;
   loading = false;
   submitted = false;
   returnUrl: string;
-  authError: boolean = false;
-  alertText = '';
   constructor(private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     public router: Router,
     private accountService: AccountService,
-    private alertService: AlertService) {
+    private toastrService: NbToastrService) {
     super(null, null, null, router);
   }
 
   ngOnInit(): void {
-    var username: string = '';
-    var password: string = '';
+    let username: string = '';
+    // let password: string = '';
     if (localStorage.getItem('username') && localStorage.getItem('rememberme') === 'true') {
       username = localStorage.getItem('username');
     //  password = CryptoJS.AES.decrypt(localStorage.getItem('password'), 'brightcast').toString();
     }
     this.form = this.formBuilder.group({
       username: [username, Validators.required],
-      password: [password, Validators.required],
-      isRememberme: [(localStorage.getItem('rememberme') === 'true' ? 'true' : '')]
+      password: ['', Validators.required],
+      isRememberme: [(localStorage.getItem('rememberme') === 'true' ? 'true' : '')],
     });
 
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    
+
   }
 
   // convenience getter for easy access to form fields
   get f() { return this.form.controls; }
 
-  onClose() {
-    this.authError = false;
-    this.alertText = '';
-  }
-
   onSubmit() {
     this.submitted = true;
-
-    // reset alerts on submit
-    this.alertService.clear();
 
     // stop here if form is invalid
     if (this.form.invalid) {
@@ -65,9 +56,6 @@ export class LoginComponent extends NbLoginComponent {
     // set rememberme in localStorage
     localStorage.setItem('rememberme', this.f.isRememberme.value);
 
-    this.authError = false; 
-    this.alertText = '';
-
     this.loading = true;
     this.accountService.login(this.f.username.value, this.f.password.value)
         .pipe(first())
@@ -76,13 +64,8 @@ export class LoginComponent extends NbLoginComponent {
               this.router.navigate([this.returnUrl]);
             },
             error => {
-                this.alertText = error;
-                this.authError = true;
-                setTimeout(() =>  this.onClose(), 2000);
-                this.alertService.error(error);
+                this.toastrService.danger(error, 'Error');
                 this.loading = false;
             });
-}
-
-
+    }
 }
--- brightcast.api/webapp/src/app/custom/auth/new-password/new-password.component.ts
+++ brightcast.api/webapp/src/app/custom/auth/new-password/new-password.component.ts
@@ -1,30 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService, AlertService } from '../../../pages/_services';
+import { AccountService } from '../../../pages/_services';
 import { first } from 'rxjs/operators';
 import { NbToastrService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-new-password',
   templateUrl: './new-password.component.html',
-  styleUrls: ['./new-password.component.scss']
+  styleUrls: ['./new-password.component.scss'],
 })
 export class NewPasswordComponent implements OnInit {
 
   form: FormGroup;
   loading = false;
   submitted = false;
-  authError: boolean = false;
-  alertText = '';
   code;
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private accountService: AccountService,
-    private alertService: AlertService,
-    private toastrService: NbToastrService
+    private toastrService: NbToastrService,
   ) { }
 
   ngOnInit(): void {
@@ -32,21 +29,16 @@ export class NewPasswordComponent implements OnInit {
       this.code = p['id'];
       this.form = this.formBuilder.group({
         password: ['', [Validators.required, Validators.minLength(6)]],
-        password2: ['']},{validator: this.checkPasswords });
-    })
-    
+        password2: ['']}, {validator: this.checkPasswords });
+    });
+
   }
 
-  onClose() {
-    this.authError = false;
-    this.alertText = '';
-  }
-  
   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-    let pass = group.get('password').value;
-    let confirmPass = group.get('password2').value;
+    const pass = group.get('password').value;
+    const confirmPass = group.get('password2').value;
 
-    return pass === confirmPass ? null : { notSame: true }     
+    return pass === confirmPass ? null : { notSame: true };
   }
 
   get f() { return this.form.controls; }
@@ -54,30 +46,20 @@ export class NewPasswordComponent implements OnInit {
   onSubmit() {
     this.submitted = true;
 
-    // reset alerts on submit
-    this.alertService.clear();
-
     // stop here if form is invalid
     if (this.form.invalid) {
       return;
     }
 
-    this.authError = false; 
-    this.alertText = '';
-
     this.loading = true;
     this.accountService.resetPassword(this.f.password.value, this.code)
         .subscribe(
-            data => {                    
-                this.toastrService.success("✔ The Password Has been reseted!", "Success!");
+            data => {
+                this.toastrService.success('✔ The Password Has been reseted!', 'Success!');
                 this.router.navigate(['auth/password/confirm'], { relativeTo: this.route });
             },
             error => {
-                this.toastrService.danger("❌ The request password token is expired!", "Error!");
-                this.alertText = error;
-                  this.authError = true;
-                  setTimeout(() =>  this.onClose(), 2000);
-                this.alertService.error(error);
+                this.toastrService.danger('❌ The request password token is expired!', 'Error!');
                 this.loading = false;
             });
   }
